3821eaff41c4da192c8c3d5917668191a97a67d0
Axel Legallais-Korsbakken <axelle@stud.ntnu.no>
2022-02-17 15:27:47 +0100

    add user class with fields

--- turapp/base/models.py
+++ turapp/base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    userName = models.CharField(max_length=20)
+    userID = models.AutoField
+    userMail = models.EmailField
+    name = models.CharField
+    userPic = models.ImageField
