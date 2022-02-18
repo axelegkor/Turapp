314e0738e5b7f3619db07107007cd101b737489a
Eirin Sudbø Sivertsen <eirinsivertsen@Eirins-MacBook-Pro.local>
2022-02-18 11:38:59 +0100

    Added user login and logout + imports

--- turapp/base/views.py
+++ turapp/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
