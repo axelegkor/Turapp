d4f766abbc219a02534266fc2ee70608c924907f
Axel Legallais-Korsbakken <axelle@stud.ntnu.no>
2022-02-22 12:28:19 +0100

    comment out code that stops code from running

--- turapp/base/models.py
+++ turapp/base/models.py
@@ -1,3 +1,4 @@
+'''
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from models import Hike
@@ -32,3 +33,4 @@ class Hike(models.Model):
 
     def __str__(self):
         return self.title
+'''
