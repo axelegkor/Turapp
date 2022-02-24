a221de2becc47ee84f19fef799923cef85b717fd
Eirin Sudbø Sivertsen <eirinsivertsen@dhcp-10-24-4-144.wlan.ntnu.no>
2022-02-24 14:55:13 +0100

    Added basic access tests to test_views.
    Co-authored-by: Sophie sophiex@stud.ntnu.no

--- turapp/base/tests/test_views.py
+++ turapp/base/tests/test_views.py
@@ -2,19 +2,20 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 
-""" class BaseTest(TestCase):
-    def setUp(self):
-        client = Client() """
 
-
-
-""" class LoginTest(TestCase):
+class AccessTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.login_url = reverse('login')
+        self.register_url = reverse('register')
     
 
-    def test_user_can_access_page(self):
+    def test_user_can_access_loginPage(self):
         response = self.client.get(self.login_url)
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'templates/base/login.html') """
+        self.assertTemplateUsed(response, 'base/login.html')
+
+    def test_user_can_access_registerPage(self):
+        response = self.client.get(self.register_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base/register.html')
