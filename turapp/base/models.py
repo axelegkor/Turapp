dc7bdcafc60e2853c24d8e527f138a89a78225d2
Axel Legallais-Korsbakken <axelle@stud.ntnu.no>
2022-02-24 15:15:13 +0100

    done alot, but get error

--- turapp/base/models.py
+++ turapp/base/models.py
@@ -1,24 +1,28 @@
+from email.policy import default
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
-class User(AbstractUser):
-    userID = models.AutoField
-    #username = models.CharField(max_length=20)
-    userMail = models.EmailField
-    name = models.CharField(max_length=30)
-    userPic = models.ImageField
+# class User(AbstractUser):
+#userID = models.AutoField
+#username = models.CharField(max_length=20)
+#userMail = models.EmailField
+#name = models.CharField(max_length=30)
+#picture = models.ImageField
 
 
 class Hike(models.Model):
-    hikeID = models.AutoField
+    #hikeID = models.AutoField
     title = models.CharField(max_length=30)
     host = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="hosting")
-    description = models.TextField
-    scheduled = models.DateTimeField
-    picture = models.ImageField
-    meetup = models.CharField
+    description = models.TextField(
+        default="Arrangøren har ikke lagt ved noen beskrivelse")
+    scheduled = models.DateTimeField(default=None, blank=True, null=True)
+    picture = models.ImageField(default=None)
+    meetup = models.CharField(
+        max_length=50, default="Arrangøren har ikke valgt et oppmøtested")
     participants = models.ManyToManyField(User, related_name="hikes")
 
     def joinHike(self, user):
--- turapp/base/views.py
+++ turapp/base/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import User
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
